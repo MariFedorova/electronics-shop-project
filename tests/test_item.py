ddc4d1815d1b0c39a4bc0afd5111e7c36e0abd41
Mariia Fedorova <000maria.fed@gmail.com>
2023-06-28 19:04:29 +0300

    dz 3

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,9 +2,11 @@
 from src.item import Item
 import pytest
 
+
 @pytest.fixture
 def test_item():
     return Item("Смартфон", 10000, 20)
+
 
 def test_calculate_total_price(test_item):
     test_item.calculate_total_price()
@@ -14,11 +16,20 @@ def test_calculate_total_price(test_item):
 def test_apply_discount(test_item):
     test_item.apply_discount()
     assert test_item.price == 8000
-def test_string_to_number():
 
+
+def test_string_to_number():
     assert Item.string_to_number('5.0') == 5
 
-def test_name(test_item):
 
+def test_name(test_item):
     test_item.name = 'СуперСмартфон'
     assert test_item.name == 'СуперСмарт'
+
+
+def test_repr(test_item):
+    assert repr(test_item) == "Item('Смартфон', 10000, 20)"
+
+
+def test_str(test_item):
+    assert str(test_item) == 'Смартфон'
